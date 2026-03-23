7ef80776c95db13fa5842e945a1d32ed865838b3
bankragilgunawan <bankragilgunawan@gmail.com>
2026-03-23 15:48:00 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,19 +6,15 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    // BARIS DI BAWAH INI SANGAT PENTING
-    base: './pulsa2saldo/', 
+    base: '/pulsa2saldo/', // Harus pakai garis miring di awal dan akhir
     plugins: [react(), tailwindcss()],
     define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY),
     },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
     },
   };
 });
